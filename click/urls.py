6203812e5eb587164bd644f79158de2a75ae67fe
Benjamingik <alisheryuldoshev44@gmail.com>
2023-12-25 20:41:39 +0500

    Fixed 25.12.23 20:41

--- click/urls.py
+++ click/urls.py
@@ -16,5 +16,3 @@ urlpatterns = [
     path('variant/create/', VariantCreateView.as_view(), name='variant_create')
     # path('/<int:pk>')
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
